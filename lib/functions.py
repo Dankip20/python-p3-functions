01c64684af2541299797fa10b7d6726d4c791c9d
Daniel Kiptoo <kiptoodaniel92@gmail.com>
2024-05-25 14:16:43 +0300

    created functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
+    
 
 def greet(name):
-    pass
-
+     print(f"Hello, {name}!")  
+     pass
+    
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(1, 1)
 
 def halve(number):
-    pass
+    return number / 2
+halve(100)
